b982c8601f9fc81b3ea088f57aa20db568fafd29
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-11-08 19:56:28 -0700

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
+  namespace :api do
+    namespace :v1 do
+      post "/customer_subscriptions", to: "customer_subscriptions#create"
+
+      # get "/customer_subscriptions/:customer_id", to: "customer_subscriptions#index"
+    end
+  end
 end
